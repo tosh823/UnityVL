9eb99449fa472870b5ac12ffc1a4beb2681680d3
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-11-22 16:00:10 +0200

    Completed map

---
+++ Assets/Scripts/WayPointMap/NavDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WayPointMap {
+    public class NavDrawer : MonoBehaviour {
+
+        private WayPoint point;
+   
+        void Start() {
+            point = GetComponentInParent<WayPoint>();
+        }
+
+        void Update() {
+            if (point != null) {
+                foreach (WayPoint linkedNode in point.neighbors) {
+                    if (linkedNode != null) {
+                        Debug.DrawLine(transform.position, linkedNode.transform.position, Color.cyan);
+                    }
+                }
+            }
+        }
+    }
+}
