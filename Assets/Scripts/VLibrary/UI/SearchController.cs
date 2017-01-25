397caa0fd6b72107b81ff25d7957bd0cb62bcf08
Ilya Minyaev <ayli.veaynim@gmail.com>
2017-01-25 18:26:12 +0200

    Better buttons

---
+++ Assets/Scripts/VLibrary/UI/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VLibrary {
+    public class SearchController : MonoBehaviour {
+
+        private bool folded = true;
+        private RectTransform rect;
+        private Vector2 defaultSize;
+        
+        void Start() {
+            rect = GetComponent<RectTransform>();
+            defaultSize = rect.sizeDelta;
+            Debug.Log("Default delta = " + defaultSize);
+            Debug.Log("Default delta multiplied = " + defaultSize*2);
+        }
+        
+        void Update() {
+
+        }
+
+        public void onIconClick() {
+            if (folded) unfold();
+            else fold();
+        }
+
+        private void fold() {
+            folded = true;
+            rect.sizeDelta = defaultSize;
+        }
+
+        private void unfold() {
+            folded = false;
+            rect.sizeDelta = new Vector2(100, 100);
+        }
+    }
+}
