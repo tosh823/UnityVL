473df75c8661a4b132e0a8a5e46b271f33b463a1
Ilya Minyaev <ayli.veaynim@gmail.com>
2017-02-01 11:55:30 +0200

    Fixed item list view

--- Assets/Scripts/VLibrary/UI/ItemListView.cs
+++ Assets/Scripts/VLibrary/UI/ItemListView.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VLibrary {
     public class ItemListView : MonoBehaviour {
 
         public GameObject itemPrefab;
-        public Transform content;
 
         private List<ItemView> items;
 
         void Start() {
             items = new List<ItemView>();
-            Debug.Log(content);
         }
 
         void Update() {
@@ -22,10 +21,19 @@ namespace VLibrary {
         public void Populate(List<Book> books) {
             Clear();
             items = new List<ItemView>();
+            RectTransform content = GetComponent<ScrollRect>().content;
+            float itemHeight = itemPrefab.GetComponent<LayoutElement>().preferredHeight;
+            float width = content.sizeDelta.x;
+            content.sizeDelta = new Vector2(width, itemHeight * books.Count);
             for (int i = 0; i < books.Count; i++) {
                 GameObject newItem = Instantiate(itemPrefab) as GameObject;
                 newItem.name = "BookItem " + i;
                 newItem.transform.SetParent(content, false);
+
+                /*RectTransform rectTransform = newItem.GetComponent<RectTransform>();
+                float vertShift = rectTransform.rect.height * i;
+                rectTransform.offsetMin = new Vector2(0, vertShift);*/
+
                 ItemView view = newItem.GetComponent<ItemView>();
                 view.UpdateContent(books[i]);
                 items.Add(view);
