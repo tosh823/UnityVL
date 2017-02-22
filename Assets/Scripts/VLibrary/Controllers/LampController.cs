709b2769ad51dcdf7b28b8a7d65c20decbc3cf1b
Ilya Minyaev <ayli.veaynim@gmail.com>
2017-02-22 14:44:53 +0200

    Turn on and off lamps

---
+++ Assets/Scripts/VLibrary/Controllers/LampController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VLibrary {
+    public class LampController : MonoBehaviour {
+
+        private Light lamp;
+
+        void Start() {
+            lamp = GetComponentInChildren<Light>();
+        }
+
+        void Update() {
+
+        }
+
+        public void Switch() {
+            if (lamp.enabled) {
+                lamp.enabled = false;
+            }
+            else {
+                lamp.enabled = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/VLibrary/Player/AvatarController.cs
+++ Assets/Scripts/VLibrary/Player/AvatarController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 namespace VLibrary {
     public class AvatarController : MonoBehaviour {
 
+        public float interactionDistance = 10f;
+
         private FPSControl control;
+        private Camera head;
 
         void Start() {
             control = GetComponent<FPSControl>();
+            head = GetComponentInChildren<Camera>();
         }
 
         void Update() {
-            
+            RaycastHit hit;
+            Ray ray = head.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if (Physics.Raycast(ray, out hit, interactionDistance)) {
+                Transform objectHit = hit.transform;
+                if (objectHit.GetComponent<LampController>() != null && Input.GetKeyUp(KeyCode.E)) {
+                    objectHit.GetComponent<LampController>().Switch();
+                }
+            }
         }
 
         public void enableControl(bool enabled) {
